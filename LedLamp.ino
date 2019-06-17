aec0f54b3cd18285d221856fdf497bdb3ed07d2d
Thomas Vachuska <tom@opennetworking.org>
2019-06-17 12:54:07 -0700

    Separating effect data per strip

--- LedLamp.ino
+++ LedLamp.ino
@@ -43,6 +43,7 @@ struct StripRec {
     Pattern *pattern;
     uint8_t hue;
     CLEDController *ctl;
+    byte    data[LED_COUNT];
 };
 
 Strip front = { .name = "front", .on = true, .color = CRGB::White, .brightness = BRIGHTNESS, .leds = &frontLeds[0] };
@@ -257,31 +258,27 @@ void juggle(Strip *s) {
 #define SPARKING 100
 
 void fire(Strip *s) {
-    // Array of temperature readings at each simulation cell
-    static byte heat[LED_COUNT];
-
     // Step 1.  Cool down every cell a little
     for (int i = 0; i < LED_COUNT; i++) {
-        heat[i] = qsub8(heat[i], random8(0, ((COOLING * 10) / LED_COUNT) + 2));
+        s->data[i] = qsub8(s->data[i], random8(0, ((COOLING * 10) / LED_COUNT) + 2));
     }
 
     // Step 2.  Heat from each cell drifts 'up' and diffuses a little
     for (int k = LED_COUNT - 1; k >= 2; k--) {
-        heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+        s->data[k] = (s->data[k - 1] + s->data[k - 2] + s->data[k - 2]) / 3;
     }
 
     // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
     if (random8() < SPARKING) {
         int y = random8(7);
-        heat[y] = qadd8(heat[y], random8(160, 255));
+        s->data[y] = qadd8(s->data[y], random8(160, 255));
     }
 
     // Step 4.  Map from heat cells to LED colors
     for (int j = 0; j < LED_COUNT; j++) {
-        s->leds[j] = HeatColor(heat[j]);
+        s->leds[j] = HeatColor(s->data[j]);
     }
 }
-
 
 
 
