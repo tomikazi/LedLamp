16f061697dcc3bca12e3109253bbbc03e9c279e4
Thomas Vachuska <tom@opennetworking.org>
2020-04-02 16:06:08 -0700

    Upgraded FastLED library.

--- LedLamp.ino
+++ LedLamp.ino
@@ -8,7 +8,7 @@
 
 #define LED_LIGHTS      "LedLamp"
 #define SW_UPDATE_URL   "http://iot.vachuska.com/LedLamp.ino.bin"
-#define SW_VERSION      "2020.03.31.001"
+#define SW_VERSION      "2020.04.01.001"
 
 #define STATE      "/cfg/state"
 #define FAVS       "/cfg/favs"
